4c2696d89f0d763fa7389b899632a03f2ccb54dd
Omkar1250 <shindadeomkar@gmail.com>
2024-05-19 23:58:40 +0530

    Netlify code push

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ dbConnect();
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'production' ? "https://your-netlify-site.netlify.app" : "http://localhost:3000",
+    origin: process.env.NODE_ENV === 'production' ? "https://https://codeshuttle.netlify.app/" : "http://localhost:3000",
     credentials: true,
   })
 );
